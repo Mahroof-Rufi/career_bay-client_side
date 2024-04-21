ac42816f1d290cedeeaa5b1d4b591b319ff0573c
Mahroof <mahroofprsnl@gmail.com>
2024-04-21 18:11:27 +0530

    navbar initail design ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: "#FF6B00"
+      },
+    },
   },
   plugins: [],
 }
