c65560ba4387eb089f669e5041656b5dbcd611dc
Mahroof_Rufi <135498710+Mahroof-Rufi@users.noreply.github.com>
2026-04-19 17:09:41 +0530

    fixed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     defaultAvatarApi: 'https://avatar.iran.liara.run/',
     serverURL: 'https://carrer-bay-server-side.onrender.com',
-    baseURL: 'api/',
+    baseURL: 'https://carrer-bay-server-side.onrender.com/api/',
     googleClientId: '1030200020686-7dolvpabdb7oc0v66ukaabpok6uk4v6c.apps.googleusercontent.com',
 };
